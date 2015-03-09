321006430fe90734a99042da6d1360633bcd136e
Ryan Spittler <ryanspittler@gmail.com>
2015-03-08 21:43:40 -0600

    Added Time and Introduce
    - Added routes for time/now and introduce/name1/and/name2. They now
      point to the right controller.
    - Added actions for time and introduce in Welcome. They now point
      to the right views.
    - Added view for time. It shows the current time when the page is
      loaded.
    - Added view for introduce. If there are the required two names, it
      introduces the two to each other.
    - Did not branch, per instructor request. Committing to the master
      branch.
    
    [#89844412]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   get 'hello' => 'welcome#hello'
   get 'hello/:name' => 'welcome#hello'
+  get 'time' => 'welcome#time'
+  get 'time/now' => 'welcome#time'
+  get 'introduce' => 'welcome#introduce'
+  get 'introduce/:name1' => 'welcome#introduce'
+  get 'introduce/:name1/and' => 'welcome#introduce'
+  get 'introduce/:name1/and/:name2' => 'welcome#introduce'
 end
